cd7c02fca85639f34bf17e1ccf75da227afc0ce2
Saeed Kassab <saeedkassab2000@gmail.com>
2025-09-15 17:51:19 +0200

    Fix bug.

--- build.sh
+++ build.sh
@@ -1,3 +1,3 @@
-javac -d poker_solitaire/bin poker_solitaire/cardutils/*.java poker_solitaire/ps/*.java poker_solitaire/*.java
+javac -d Poker_Solitaire/bin Poker_Solitaire/cardutils/*.java Poker_Solitaire/ps/*.java Poker_Solitaire/*.java
 cd Poker_Solitaire/bin/
 java Poker_Solitaire/Main  

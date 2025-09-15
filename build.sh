603b5120cf09c8bcff23049aac3446f12f4b3415
Saeed Kassab <saeedkassab2000@gmail.com>
2025-09-15 17:48:52 +0200

    Adjust build.sh

--- build.sh
+++ build.sh
@@ -1,3 +1,3 @@
 javac -d poker_solitaire/bin poker_solitaire/cardutils/*.java poker_solitaire/ps/*.java poker_solitaire/*.java
-cd poker_solitaire/bin/
-java poker_solitaire/Main  
+cd Poker_Solitaire/bin/
+java Poker_Solitaire/Main  
